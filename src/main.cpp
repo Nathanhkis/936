5595d91d309d765b7615097e34370ae598d9188b
Nathan Zhong <nathanzhong@Nathans-MacBook-Air-2.local>
2025-05-30 22:52:58 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,64 @@ pros::Imu imu(10);
 
 pros::Rotation rotation_sensor(1);
 
+
+
+// horizontal tracking wheel encoder
+pros::Rotation horizontal_encoder(20);
+pros::Rotation vertical_encoder(20);
+
+
+// vertical tracking wheel encoder
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_275, 0.41);
+// vertical tracking wheel
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, -0.89);
+
+
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+
+
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+
+
+
+
+
+
+
+
+
+
+
 void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
@@ -113,5 +171,16 @@ void opcontrol() {
 		left_mg.move_velocity(left_speed);   // Set left motors by speed (RPM)
 		right_mg.move_velocity(right_speed); // Set right motors by speed (RPM)
 		pros::delay(20);                     // Run for 20 ms then update
+
+		/*
+		int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+
+        // move the robot
+        chassis.arcade(leftY, rightX);
+
+        // delay to save resources
+        pros::delay(25);
+		*/
 	}
 }
